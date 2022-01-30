92f1704c8ab1b319776ae8b7b6b6ae940b4cc9a9
unknown <aleks.it.jv@gmail.com>
2022-01-30 21:23:44 +0200

    commit before new

--- src/main/java/org/example/pharmasy/configuration/WebSecurityConfig.java
+++ src/main/java/org/example/pharmasy/configuration/WebSecurityConfig.java
@@ -1,23 +1,31 @@
 package org.example.pharmasy.configuration;
 
-import org.springframework.context.annotation.Bean;
+import org.example.pharmasy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+/*    @Autowired
+    private DataSource dataSource;*/
+
+    @Autowired
+    private UserService userService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -28,7 +36,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    @Bean
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+                 auth.userDetailsService(userService)
+                /*auth.jdbcAuthentication()
+                .dataSource(dataSource)*/
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    /*          .usersByUsernameQuery("select username, password, active from usr where username=?")
+                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id=ur.user_id where u.username=?");*/
+    }
+
+
+    /*    @Bean
     @Override
     public UserDetailsService userDetailsService() {
         UserDetails user =
@@ -39,5 +58,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .build();
 
         return new InMemoryUserDetailsManager(user);
-    }
+    }*/
 }
---
+++ src/main/java/org/example/pharmasy/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.pharmasy.repository;
+
+import org.example.pharmasy.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
